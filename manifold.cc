cbdfc826e1f5757ebc7214a66f166dcebd422024
Peter Salvi <vukung@yahoo.com>
2024-03-08 11:14:59 +0100

    Interpolate center points (MP & quadratic Bezier)

--- manifold.cc
+++ manifold.cc
@@ -8,20 +8,60 @@
 #include <transfinite/surface-generalized-bezier.hh>
 #include <transfinite/surface-midpoint.hh>
 
+using namespace Geometry;
+
 struct Traits : public OpenMesh::DefaultTraits {
   using Point  = OpenMesh::Vec3d;
 };
 using Mesh = OpenMesh::PolyMesh_ArrayKernelT<Traits>;
 
-using NPatch = Transfinite::SurfaceC0Coons;
-// using NPatch = Transfinite::SurfaceGeneralizedBezier;
-// using NPatch = Transfinite::SurfaceMidpoint;
+class NPatch {
+public:
+  virtual ~NPatch() {}
+  virtual Point3D eval(const Point2D &) const = 0;
+  virtual size_t size() const = 0;
+};
 
-using namespace Geometry;
+class MP : public NPatch {
+public:
+  MP(const Transfinite::SurfaceMidpoint &mp) : patch(mp) {}
+  ~MP() {}
+  Point3D eval(const Point2D &p) const override { return patch.eval(p); }
+  size_t size() const override { return patch.domain()->size(); }
+private:
+  Transfinite::SurfaceMidpoint patch;
+};
+
+// Quadratic Bezier patch in [-1,1]^2
+class QB : public NPatch {
+public:
+  QB(const PointMatrix &curves, const Point3D &center) {
+    const auto &P00 = curves[2][2];
+    const auto &P10 = curves[3][1];
+    const auto &P20 = curves[0][0];
+    const auto &P01 = curves[2][1];
+    const auto &P21 = curves[0][1];
+    const auto &P02 = curves[1][2];
+    const auto &P12 = curves[1][1];
+    const auto &P22 = curves[0][2];
+    patch = BSSurface(2, 2, {
+        P00, P01, P02,
+        P10, (center*16-(P00+P02+P20+P22)-(P10+P01+P12+P21)*2)/4, P12,
+        P20, P21, P22
+      });
+  }
+  ~QB() {}
+  Point3D eval(const Point2D &p) const override {
+    return patch.eval(0.5 + p[0] / 2, 0.5 + p[1] / 2);
+  }
+  size_t size() const override { return 4; }
+private:
+  BSSurface patch;
+};
 
 // Generates the N-patch based on the quads around
 // the vertex at the origin of the given half-edge.
-NPatch generateNPatch(const Mesh &mesh, OpenMesh::SmartHalfedgeHandle he) {
+std::unique_ptr<NPatch> generateNPatch(const Mesh &mesh, OpenMesh::SmartHalfedgeHandle he) {
   Point3D center(mesh.point(he.from()).data());
   PointMatrix curves;
   auto start = he.next(), it = start;
@@ -36,52 +76,54 @@ NPatch generateNPatch(const Mesh &mesh, OpenMesh::SmartHalfedgeHandle he) {
   curves.insert(curves.begin(), curves.back());
   curves.pop_back();
 
-  PointMatrix opposite;
-  size_t n = curves.size();
-  for (size_t i = 0; i < n; ++i) {
-    size_t im = (i + n - 1) % n, ip = (i + 1) % n;
-    auto q0 = curves[im][1];
-    auto q1 = center;
-    auto q2 = curves[ip][1];
-    opposite.push_back({ q0, q1, q2 });
-  }
+  if (curves.size() == 4)
+    return std::make_unique<QB>(curves, center);
 
-  NPatch surface;
-#if 1
+  Transfinite::SurfaceMidpoint surface;
   std::vector<std::shared_ptr<Transfinite::Curve>> boundaries;
   std::transform(curves.begin(), curves.end(), std::back_inserter(boundaries),
                  [](const PointVector &p) {
                    return std::make_shared<Transfinite::BSplineCurve>(p);
                  });
   surface.setCurves(boundaries);
-#else
+
   // GB patch specific part
-  double a = 1.0/3.0, b = 2.0/3.0;
-  surface.initNetwork(n, 3);
+  // double a = 1.0/3.0, b = 2.0/3.0;
+  // surface.initNetwork(n, 3);
 
-  center = Point3D(0, 0, 0);
-  for (size_t i = 0; i < n; ++i)
-    center += curves[i][1];
-  center /= n;
+  // center = Point3D(0, 0, 0);
+  // for (size_t i = 0; i < n; ++i)
+  //   center += curves[i][1];
+  // center /= n;
 
-  surface.setCentralControlPoint(center);
-  for (size_t i = 0; i < n; ++i) {
-    const auto &p = curves[i];
-    const auto &q = opposite[i];
-    surface.setControlPoint(i, 0, 0, p[0]);
-    surface.setControlPoint(i, 1, 0, p[0] * a + p[1] * b);
-    surface.setControlPoint(i, 2, 0, p[2] * a + p[1] * b);
-    surface.setControlPoint(i, 3, 0, p[2]);
-    surface.setControlPoint(i, 0, 1, p[0] * a + q[0] * b);
-    surface.setControlPoint(i, 1, 1, p[0] * a * a + (p[1] + q[0]) * a * b + q[1] * b * b);
-    surface.setControlPoint(i, 2, 1, p[2] * a * a + (p[1] + q[2]) * a * b + q[1] * b * b);
-    surface.setControlPoint(i, 3, 1, p[2] * a + q[2] * b);
-  }
-#endif
+  // PointMatrix opposite;
+  // size_t n = curves.size();
+  // for (size_t i = 0; i < n; ++i) {
+  //   size_t im = (i + n - 1) % n, ip = (i + 1) % n;
+  //   auto q0 = curves[im][1];
+  //   auto q1 = center;
+  //   auto q2 = curves[ip][1];
+  //   opposite.push_back({ q0, q1, q2 });
+  // }
+
+  // surface.setCentralControlPoint(center);
+  // for (size_t i = 0; i < n; ++i) {
+  //   const auto &p = curves[i];
+  //   const auto &q = opposite[i];
+  //   surface.setControlPoint(i, 0, 0, p[0]);
+  //   surface.setControlPoint(i, 1, 0, p[0] * a + p[1] * b);
+  //   surface.setControlPoint(i, 2, 0, p[2] * a + p[1] * b);
+  //   surface.setControlPoint(i, 3, 0, p[2]);
+  //   surface.setControlPoint(i, 0, 1, p[0] * a + q[0] * b);
+  //   surface.setControlPoint(i, 1, 1, p[0] * a * a + (p[1] + q[0]) * a * b + q[1] * b * b);
+  //   surface.setControlPoint(i, 2, 1, p[2] * a * a + (p[1] + q[2]) * a * b + q[1] * b * b);
+  //   surface.setControlPoint(i, 3, 1, p[2] * a + q[2] * b);
+  // }
 
   surface.setupLoop();
   surface.update();
-  return surface;
+  surface.setMidpoint(center);
+  return std::make_unique<MP>(surface);
 }
 
 double trapezoid(const std::function<double(double)> &f, double a, double b,
@@ -126,11 +168,11 @@ double blendFunction(double x) {
   // G1 Hermite
   // return std::pow(1 - x, 3) + 3 * std::pow(1 - x, 2) * x;
   // G2 Hermite
-  return std::pow(1 - x, 5) + 5 * std::pow(1 - x, 4) * x + 10 * std::pow(1 - x, 3) * x * x;
+  // return std::pow(1 - x, 5) + 5 * std::pow(1 - x, 4) * x + 10 * std::pow(1 - x, 3) * x * x;
   // Bump
   // return std::exp(-1 / (1 - x)) / (std::exp(-1 / x) + std::exp(-1 / (1 - x)));
   // ERBS
-  // return 1 - erbsBlend(x);
+  return 1 - erbsBlend(x);
 }
 
 double blend(const Point2D &uv) {
@@ -154,7 +196,7 @@ int main(int argc, char **argv) {
   TriMesh mesh;
 
   for (auto face : cage.faces()) {
-    std::vector<NPatch> patches;
+    std::vector<std::unique_ptr<NPatch>> patches;
     for (auto he : face.halfedges())
       patches.push_back(generateNPatch(cage, he));
 
@@ -174,7 +216,7 @@ int main(int argc, char **argv) {
           case 3: uv[0] = 1 - v; uv[1] = u; break;
           }
           auto b = blend(uv);
-          auto n = surface.domain()->size();
+          auto n = surface->size();
           if (n != 4) {
             auto c = std::cos(2 * M_PI / n), s = std::sin(2 * M_PI / n);
             Point2DVector corners = {
@@ -184,7 +226,7 @@ int main(int argc, char **argv) {
             auto p2 = corners[3] + (corners[2] - corners[3]) * uv[0];
             uv = p1 + (p2 - p1) * uv[1];
           }
-          p += surface.eval(uv) * b;
+          p += surface->eval(uv) * b;
         }
         points.push_back(p);
       }
