8fdbaf861ec416a01f71c142000d15550627ad61
Peter Salvi <vukung@yahoo.com>
2024-03-15 16:36:32 +0100

    PostScript output

--- paramtest.cc
+++ paramtest.cc
@@ -4,30 +4,66 @@
 
 using namespace Geometry;
 
+#define PS_OUTPUT
+
+#ifdef PS_OUTPUT
+
+void init() {
+  std::cout << "%!PS" << std::endl;
+}
+void writeSegments(const PointVector &pv) {
+  std::cout << "newpath" << std::endl;
+  size_t n = pv.size();
+  for (size_t k = 0; k < n; ++k) {
+    const auto &p = pv[k];
+    auto x = p[0] * 200 + 250;
+    auto y = 400 - p[1] * 200;
+    std::cout << x << ' ' << y << (k ? " lineto" : " moveto") << std::endl;
+  }
+  std::cout << "stroke" << std::endl;
+}
+void finalize() {
+  std::cout << "showpage" << std::endl;
+}
+
+#else // !PS_OUTPUT
+
+void init() { }
+void writeSegments(const PointVector &pv) {
+  static size_t index = 1;
+  size_t n = pv.size();
+  for (const auto &p : pv)
+    std::cout << "v " << p << std::endl;
+  std::cout << 'l';
+  for (size_t k = 0; k < n; ++k)
+    std::cout << ' ' << index + k;
+  std::cout << std::endl;
+  index += n;
+}
+void finalize() { }
+
+#endif
+
 int main() {
   bool u_dir = true;
-  size_t n = 5, res = 10;
+  size_t n = 5, res = 5;
 
-  size_t index = 1;
+  init();
 
-  if (false) {                   // domain polygon
-    for (size_t k = 0; k <= n; ++k) {
-      Point3D p(std::cos(2 * k * M_PI / n), std::sin(2 * k * M_PI / n), 1);
-      std::cout << "v " << p << std::endl;
-    }
-    std::cout << 'l';
+  if (true) {                   // domain polygon
+    PointVector pv;
     for (size_t k = 0; k <= n; ++k)
-      std::cout << ' ' << index + k;
-    std::cout << std::endl;
-    index += n + 1;
+      pv.emplace_back(std::cos(2 * k * M_PI / n), std::sin(2 * k * M_PI / n), 1);
+    writeSegments(pv);
   }
 
-  if (false) {                   // bilinear mapping
+  if (true) {                   // bilinear mapping
     auto c = std::cos(2 * M_PI / n), s = std::sin(2 * M_PI / n);
     PointVector corners = {
       { 0, 0, 1 }, { 0.5 + c / 2, -s / 2, 1 }, { 1, 0, 1 }, { 0.5 + c / 2, s / 2, 1 }
     };
     for (size_t i = 0; i <= res; ++i) {
+      PointVector pv;
       auto u = i / (double)res;
       for (size_t j = 0; j <= 100; ++j) {
         auto v = j / 100.0;
@@ -35,17 +71,13 @@ int main() {
           std::swap(u, v);
         auto p1 = corners[0] + (corners[1] - corners[0]) * u;
         auto p2 = corners[3] + (corners[2] - corners[3]) * u;
-        auto p = p1 + (p2 - p1) * v;
+        pv.push_back(p1 + (p2 - p1) * v);
         if (!u_dir)
           std::swap(u, v);
-        std::cout << "v " << p << std::endl;
       }
-      std::cout << 'l';
-      for (size_t j = 0; j <= 100; ++j)
-        std::cout << ' ' << index + j;
-      std::cout << std::endl;
-      index += 101;
+      writeSegments(pv);
     }
+    finalize();
     return 0;
   }
 
@@ -60,17 +92,11 @@ int main() {
     { c, s, 1 },
     { (c2 + c) / 2, (s2 + s) / 2, 1 }
   };
-  if (u_dir)
-    std::cout << "v " << pv[0] << std::endl;
-  else
-    std::cout << "v " << pv[6] << std::endl;
-  std::cout << "v 0 0 1" << std::endl;
-  if (u_dir)
-    std::cout << "v " << pv[4] << std::endl;
-  else
-    std::cout << "v " << pv[2] << std::endl;
-  std::cout << "l 1 2 3" << std::endl;
-  index += 3;
+  PointVector points;
+  points.push_back(u_dir ? pv[0] : pv[6]);
+  points.emplace_back(0, 0, 1);
+  points.push_back(u_dir ? pv[4] : pv[2]);
+  writeSegments(points);
   for (size_t i = 1; i <= res; ++i) {
     auto u = i / (double)res;
     BSCurve curve;
@@ -87,15 +113,14 @@ int main() {
           pv[2] * (1 - u) + pv[3] * u
         });
     curve.controlPoints()[1][2] = 1 / u;
+    points.clear();
     for (size_t j = 0; j <= 100; ++j) {
       auto v = j / 100.0;
       auto p = curve.eval(v);
-      std::cout << "v " << p[0] / p[2] << ' ' << p[1] / p[2] << " 1" << std::endl;
+      points.emplace_back(p[0] / p[2], p[1] / p[2], 1);
     }
-    std::cout << 'l';
-    for (size_t j = 0; j <= 100; ++j)
-      std::cout << ' ' << index + j;
-    std::cout << std::endl;
-    index += 101;
+    writeSegments(points);
   }
+
+  finalize();
 }
